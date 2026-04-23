e9e01051920b1b8f5557d3ae046549dced1bf2a9
Montassar Lassoued <montassar.lassoued.4231@gmail.com>
2026-04-23 17:27:49 +0200

    menu.component optimiert: +Track By +Flexbox-Layout +Style Binding

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit, Output, Input, EventEmitter } from '@angular/core';
-import { MenuService, BrowserMenu } from './menu.service';
+import { Component, Output, Input, EventEmitter, ChangeDetectionStrategy } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { BrowserMenu } from './menu.service';
 
 @Component({
   selector: 'app-menu',
@@ -9,17 +9,21 @@ import { CommonModule } from '@angular/common';
   imports: [CommonModule, RouterModule],
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css'],
+  // Performance: Nur rendern, wenn sich Inputs ändern
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class MenuComponent {
   @Input() menues: BrowserMenu[] = [];
   @Output() menuClick = new EventEmitter<BrowserMenu>();
 
   toggle(m: BrowserMenu) {
+    // Da wir OnPush nutzen, ist eine direkte Mutation (m.expanded = !m.expanded)
+    // manchmal problematisch. Besser: Das Objekt kurz "berühren" oder Signals nutzen.
     m.expanded = !m.expanded;
   }
 
   onItemClick(item: BrowserMenu, event: Event) {
-    event.stopPropagation();
+    event.stopPropagation(); // Verhindert das Auslösen von Parent-Events
     this.menuClick.emit(item);
   }
 }
